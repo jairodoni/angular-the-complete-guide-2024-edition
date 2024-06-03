4cec8512b1a6262b2f991eb5f1a0f75abd5e7a9e
Jairo Doni <jairo.doni97@gmail.com>
2024-06-03 20:00:18 -0300

    aulas de rotas em angular

---
+++ routing/src/app/servers/edit-server/can-desactivate-guard.service.ts
@@ -0,0 +1,24 @@
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  CanDeactivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+export class CanComponentDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+export class CanDeactivateGuard
+  implements CanDeactivate<CanComponentDeactivate>
+{
+  canDeactivate(
+    component: CanComponentDeactivate,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return component.canDeactivate();
+  }
+}
---
+++ routing/src/app/servers/server/server-resolver.service.ts
@@ -0,0 +1,27 @@
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  ResolveFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { ServersService } from '../servers.service';
+import { Injectable } from '@angular/core';
+
+interface Server {
+  id: number;
+  name: string;
+  status: string;
+}
+
+@Injectable()
+export class ServerResolver implements Resolve<Server> {
+  constructor(private serversService: ServersService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Server> | Promise<Server> | Server {
+    return this.serversService.getServer(+route.params['id']);
+  }
+}
